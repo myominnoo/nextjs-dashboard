7f52dec2fa1740a4c6020dedba63a4275cb4789e
Myo Minn Oo <dr.myominnoo@gmail.com>
2026-03-26 22:36:11 -0500

    implementing invoice search function

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,38 @@
+import React, { Suspense } from "react";
+import { lusitana } from "../../ui/fonts";
+import CardWrapper, { Card } from "../../ui/dashboard/cards";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { revenue } from "../../lib/placeholder-data";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import {
+  CardSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+
+const page = async () => {
+  return (
+    <main>
+      <h1
+        className={`${lusitana.className} mb-4 text-xl md:text-2xl font-bold`}
+      >
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+};
+
+export default page;
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,20 +8,21 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { neon } from '@neondatabase/serverless';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = neon(process.env.DATABASE_URL!);
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -32,6 +33,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
