5e2cb5ed72487ce5c3e163a8b0d54bf069024cf1
Myo Minn Oo <dr.myominnoo@gmail.com>
2026-03-27 21:51:35 -0500

    added error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { customers } from "@/app/lib/placeholder-data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
 import React from "react";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -12,6 +13,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
